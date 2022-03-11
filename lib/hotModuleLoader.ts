584179195a3cc596aabae75cf60d27844768bf06
njzy <njzydark@gmail.com>
2022-03-11 22:54:04 +0800

    fix: source map not work

--- lib/hotModuleLoader.ts
+++ lib/hotModuleLoader.ts
@@ -1,3 +1,9 @@
+// copied from https://github.dev/pmmmwh/react-refresh-webpack-plugin/blob/main/loader/index.js
+
+import { SourceMapConsumer, SourceNode } from 'source-map';
+
+import { getIdentitySourceMap } from './utils/getIdentitySourceMap';
+
 const runtime = require.resolve('./runtime').replace(/\\/g, '/');
 
 const ReactRefreshHotModuleInjection = `
@@ -10,10 +16,50 @@ if (module.hot && exports && runtime.isReactRefreshBoundary && runtime.isReactRe
 }
 `;
 
-function ReactRefreshHotModuleLoader(source) {
+function ReactRefreshHotModuleLoader(source, inputSourceMap, meta) {
   // @ts-ignore
   this.cacheable();
-  return source + ReactRefreshHotModuleInjection;
+
+  // @ts-ignore
+  const callback = this.async();
+
+  /**
+   * @this {import('webpack').loader.LoaderContext}
+   * @param {string} source
+   * @param {import('source-map').RawSourceMap} [inputSourceMap]
+   * @returns {Promise<[string, import('source-map').RawSourceMap]>}
+   */
+  async function _loader(source, inputSourceMap) {
+    // @ts-ignore
+    if (this.sourceMap) {
+      let originalSourceMap = inputSourceMap;
+      if (!originalSourceMap) {
+        // @ts-ignore
+        originalSourceMap = getIdentitySourceMap(source, this.resourcePath);
+      }
+
+      return SourceMapConsumer.with(originalSourceMap, undefined, consumer => {
+        const node = SourceNode.fromStringWithSourceMap(source, consumer);
+
+        node.add(['\n\n', ReactRefreshHotModuleInjection]);
+
+        const { code, map } = node.toStringWithSourceMap();
+        return [code, map.toJSON()];
+      });
+    } else {
+      return [[source, ReactRefreshHotModuleInjection].join('\n\n'), inputSourceMap];
+    }
+  }
+
+  // @ts-ignore
+  _loader.call(this, source, inputSourceMap).then(
+    ([code, map]) => {
+      callback(null, code, map, meta);
+    },
+    error => {
+      callback(error);
+    }
+  );
 }
 
 module.exports = ReactRefreshHotModuleLoader;
